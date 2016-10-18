e6ba9e7029f5bc851aac6e4efce167cb0210934f
shubhang-arora <sa287@snu.edu.in>
2016-10-18 16:52:14 +0530

    Sending msgs to console

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 var app = require('express')();
-var http = require('http').Server(app);
-
+var server	 = require('http').Server(app);
+var io = require('socket.io')(server);
 app.get('/', function(request, response){
   response.sendFile(__dirname + '/index.html')
 });
 
-http.listen(3000, function(){
+server.listen(3000, function(){
   console.log('listening on *:3000');
+});
+
+io.on('connection', function(socket){
+	console.log("connection was made");
+	socket.on('chat.message', function(message){
+		console.log(message);
+	});
 });
