420b1227b1a304c5cbf4f80f5b3e410374ebff16
shubhang-arora <sa287@snu.edu.in>
2016-10-19 18:56:48 +0530

    Broadcasting a disconnect msg wheneven a user disconnects

--- index.js
+++ index.js
@@ -7,13 +7,15 @@ app.get('/', function(req, res){
 });
 
 io.on('connection',function(socket){
-	console.log('a user connected');
+	socket.on('user',function(user){
+		io.emit('user',user);
+	});
 	socket.on('disconnect', function(){
-		console.log('u user disconnected');
+		io.emit('user','disconnected');
 	});
 	socket.on('chat-message', function(msg){
-		console.log("New message arrived "+msg);
-	})
+		io.emit('chat-message',msg);
+	});
 });
 
 http.listen(3000, function(){
