90158dd928757050014210e615e02df8ccac7df4
shubhang-arora <sa287@snu.edu.in>
2016-10-19 18:29:51 +0530

    Bug removed

--- index.js
+++ index.js
@@ -1,17 +1,21 @@
 var app = require('express')();
-var server	 = require('http').Server(app);
-var io = require('socket.io')(server);
-app.get('/', function(request, response){
-  response.sendFile(__dirname + '/index.html')
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
 });
 
-server.listen(3000, function(){
-  console.log('listening on *:3000');
-});
-
-io.on('connection', function(socket){
-	console.log("connection was made");
-	socket.on('chat.message', function(message){
-		console.log(message);
+io.on('connection',function(socket){
+	console.log('a user connected');
+	socket.on('disconnect', function(){
+		console.log('u user disconnected');
 	});
+	socket.on('chat-message', function(msg){
+		console.log("New message arrived "+msg);
+	})
 });
+
+http.listen(3000, function(){
+  console.log('listening on *:3000');
+});		
